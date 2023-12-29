847ce9a42175bd3fc3b89969baeb8b3c8bd75133
AloeThron <lxe.tr.deat@gmail.com>
2023-12-29 21:37:00 +0700

    set authProvider

---
+++ cmstock/src/app/_components/common/AuthProvider.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { usePathname, useRouter } from "next/navigation";
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function AuthProvider({ children }: Props) {
+  const router = useRouter();
+  const path = usePathname();
+
+  if (path != "/stock") {
+    router.push("/stock");
+  } else {
+    return <div>{children}</div>;
+  }
+}
