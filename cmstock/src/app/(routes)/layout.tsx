c2ede6b2c753e0e88d49b6c8838e14b1ea1fd582
AloeThron <lxe.tr.deat@gmail.com>
2023-12-30 00:44:47 +0700

    fix header and sidebar

--- cmstock/src/app/(routes)/layout.tsx
+++ cmstock/src/app/(routes)/layout.tsx
@@ -9,26 +9,6 @@ type Props = {
   children: React.ReactNode;
 };
 
-const drawerWidth = 240;
-const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })<{
-  open?: boolean;
-}>(({ theme, open }) => ({
-  flexGrow: 1,
-  padding: theme.spacing(3),
-  transition: theme.transitions.create("margin", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  marginLeft: `-${drawerWidth}px`,
-  ...(open && {
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    marginLeft: 0,
-  }),
-}));
-
 export default function layout({ children }: Props) {
   const [open, setOpen] = useState(true);
 
@@ -46,10 +26,8 @@ export default function layout({ children }: Props) {
         <Header handleDrawerOpen={handleDrawerOpen} open={open} />
         <Sidebar handleDrawerClose={handleDrawerClose} open={open} />
         <Box>
-          <Main open={open}>
             <DrawerHeader />
             {children}
-          </Main>
         </Box>
       </Box>
     </section>
